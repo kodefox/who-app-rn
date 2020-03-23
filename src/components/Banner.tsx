ee7bd2575aef55af9ffb818829471cd62734c419
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2020-03-23 17:24:30 +0700

    feat: add protect yourself scene (#21)
    * feat: add Banner component
    
    * feat: add ProtectYourselfCarousel
    
    * feat: add ProtectYourself scene

---
+++ src/components/Banner.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Image, View, StyleSheet, TouchableOpacity } from 'react-native';
+
+// TODO: Replace BannerLogo with higher res asset or use actual svg.
+import BannerLogo from '../../assets/banner_logo.png';
+import Close from '../../assets/close.svg';
+
+type Props = {
+  onClosePress?: () => void;
+};
+
+function Banner(props: Props) {
+  let { onClosePress } = props;
+  return (
+    <View style={styles.root}>
+      <Image resizeMode="contain" source={BannerLogo} style={styles.logo} />
+      {onClosePress && (
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={onClosePress}
+          style={styles.closeWrapper}
+        >
+          <Close />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+}
+
+let styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    height: 72,
+    minHeight: 72,
+    maxHeight: 72,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: 12,
+  },
+  logo: {
+    // The logo assets have white spaces in them, so we don't add margin left here.
+    width: 250,
+    height: 60,
+  },
+  closeWrapper: {
+    width: 36,
+    marginRight: 12,
+  },
+});
+
+export default Banner;
---
+++ src/components/ProtectYourselfCarousel.tsx
@@ -0,0 +1,94 @@
+import React, { useState, ReactNode, ComponentProps } from 'react';
+import { StyleSheet, View, Image as ImageBase } from 'react-native';
+
+import Carousel from './Carousel';
+import Text from './Text';
+
+import ProtectWashHand from '../../assets/protect_wash_hand.png';
+import ProtectCough from '../../assets/protect_cough.png';
+import ProtectTrashBin from '../../assets/protect_trash_bin.png';
+import ProtectNoCrowd from '../../assets/protect_no_crowd.png';
+import ProtectMedicalBag from '../../assets/protect_medical_bag.png';
+
+function ProtectYourselfCarousel() {
+  let [currentIndex, setCurrentIndex] = useState(0);
+  // TODO: Add navigation from getting started to main screen for first time user.
+  return (
+    <Carousel currentIndex={currentIndex} onIndexChanged={setCurrentIndex}>
+      <Wrapper>
+        <Text style={styles.caption} size={24}>
+          {t('Wash your hands often with soap and running water frequently')}
+        </Text>
+        <Image source={ProtectWashHand} />
+      </Wrapper>
+      <Wrapper>
+        <Text style={styles.caption} size={24}>
+          {t(
+            'When coughing and squeezing cover mouth and nose with flexed elbow or tissue',
+          )}
+        </Text>
+        <Image source={ProtectCough} />
+      </Wrapper>
+      <Wrapper>
+        <Text style={styles.caption} size={24}>
+          {t('Throw tissue into closed bin immediately after use')}
+        </Text>
+        <Image source={ProtectTrashBin} />
+      </Wrapper>
+      <Wrapper>
+        <Text style={styles.caption} size={24}>
+          {t('Wash your hands again, frequently')}
+        </Text>
+        <Image source={ProtectWashHand} />
+      </Wrapper>
+      <Wrapper>
+        <Text style={styles.caption} size={24}>
+          {t('Avoid close contact and keep the physical distancing')}
+        </Text>
+        <Image source={ProtectNoCrowd} />
+      </Wrapper>
+      <Wrapper>
+        <Text style={styles.caption} size={24}>
+          {t(
+            'Seek medical care early if you have fever, cough and difficulty breathing',
+          )}
+        </Text>
+        <Image source={ProtectMedicalBag} />
+      </Wrapper>
+    </Carousel>
+  );
+}
+
+let styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 40,
+    paddingVertical: 60,
+  },
+  caption: {
+    lineHeight: 28,
+    marginBottom: 20,
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    aspectRatio: 1,
+  },
+});
+
+let Wrapper = (props: { children: ReactNode }) => (
+  <View style={styles.wrapper} {...props} />
+);
+
+let Image = (props: ComponentProps<typeof ImageBase>) => (
+  <View style={styles.imageContainer}>
+    <ImageBase resizeMode="contain" style={styles.image} {...props} />
+  </View>
+);
+
+export default ProtectYourselfCarousel;
