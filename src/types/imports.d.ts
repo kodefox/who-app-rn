f37e6b1aec299ca841bc776b0b2e45c37817ec68
Simon <sstur@me.com>
2020-03-22 19:41:51 -0700

    Add support for SVG

---
+++ src/types/imports.d.ts
@@ -0,0 +1,8 @@
+declare module '*.jpg';
+declare module '*.png';
+
+declare module '*.svg' {
+  import { SvgProps } from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
