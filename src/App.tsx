c9766fcac4841da2e6e42669a8c401fb115924b1
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2020-03-23 13:14:20 +0700

    feat: skip getting started for returning user

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from './screens/Home';
 import GettingStartedScreen from './screens/GettingStarted';
 
+import useGettingStarted from './helpers/useGettingStarted';
+
 const Stack = createStackNavigator();
 
 function App() {
+  let isReturningUser = useGettingStarted();
+
+  if (isReturningUser == null) {
+    // TODO: Should display splash screen
+    return <ActivityIndicator />;
+  }
+
   return (
-    <View style={{ ...StyleSheet.absoluteFillObject }}>
+    <View style={StyleSheet.absoluteFill}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerBackTitleVisible: false }}>
-          <Stack.Screen
-            name="GettingStarted"
-            component={GettingStartedScreen}
-            options={{
-              headerShown: false,
-            }}
-          />
+          {!isReturningUser && (
+            <Stack.Screen
+              name="GettingStarted"
+              component={GettingStartedScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          )}
           <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
       </NavigationContainer>
