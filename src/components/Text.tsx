8857b49380ecf2059260eb8b18666931e43bd2a2
Yosua Ian Sebastian <yosua.ian.sebastian@gmail.com>
2020-03-23 12:38:14 +0700

    feat: add Text component

---
+++ src/components/Text.tsx
@@ -0,0 +1,40 @@
+import React, { ComponentProps } from 'react';
+import { Text as TextBase, StyleSheet, TextStyle } from 'react-native';
+
+type Props = ComponentProps<typeof TextBase> & {
+  size?: number;
+  weight?: TextStyle['fontWeight'];
+  fontStyle?: TextStyle['fontStyle'];
+};
+
+function Text(props: Props) {
+  let {
+    style,
+    size = 20,
+    weight = 'normal',
+    fontStyle = 'normal',
+    ...otherProps
+  } = props;
+  return (
+    <TextBase
+      style={[
+        styles.default,
+        {
+          fontSize: size,
+          fontWeight: weight,
+          fontStyle,
+        },
+        style,
+      ]}
+      {...otherProps}
+    />
+  );
+}
+
+let styles = StyleSheet.create({
+  default: {
+    lineHeight: 23,
+  },
+});
+
+export default Text;
