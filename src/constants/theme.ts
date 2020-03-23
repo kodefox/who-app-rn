e61055252fce7cce6b1d315fdc0f76d5cb09c338
Daniel Sukmana <daniel.sukmana@kodefox.com>
2020-03-23 15:33:57 +0700

    Check health (#18)
    * chore: add card background colors
    
    * chore: re-center warning svg
    
    * feat: Check Your Health carousel screen
    
    * feat: add Check Your Health screen to navigation

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -4,6 +4,8 @@
 export const colors = {
   primary: '#0093d5',
   white: '#ffffff',
+  primaryCardBackground: '#EDF6F8',
+  secondaryCardBackground: '#FAE9B0',
 };
 
 // Not sure if we need this default export.
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import theme from '../constants/theme';
+
+import Banner from '../../assets/banner_logo.svg';
 
 export default function Home() {
+  const { navigate } = useNavigation();
   return (
-    <SafeAreaView>
-      <View style={{ padding: 30 }}>
-        <Text style={{ textAlign: 'center' }}>
-          {t('Main menu will go here.')}
-        </Text>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.mainContent}>
+        <Banner />
+        <TouchableOpacity onPress={() => navigate('CheckHealth')}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>{t('Check your health')}</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
+
+let styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+  },
+  mainContent: { padding: 30 },
+  button: {
+    margin: 30,
+    padding: 20,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 4,
+    alignItems: 'center',
+  },
+  buttonText: { color: 'white' },
+});
