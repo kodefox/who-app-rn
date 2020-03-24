3b401e68ececc496b168f43a43c9874fb38b4bef
Listi <listiliz13@gmail.com>
2020-03-24 12:44:02 +0700

    feat: add share functionality (#28)
    * fix: set has finished onboarding after user finished onboarding
    
    * chore: add todo to handle sharing in web

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet, Share, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import Banner from '../components/Banner';
 import Button from '../components/Button';
+
+let onShareButtonPress = () => {
+  if (Platform.OS === 'web') {
+    //   TODO: Handle sharing for web
+    return;
+  }
+  //   TODO: Change with expo url
+  Share.share({
+    message:
+      'Check out the official COVID-19 GUIDE App https://preview.whoapp.org/menu',
+  });
+};
 
 export default function Home() {
   const { navigate } = useNavigation();
@@ -27,7 +39,11 @@ export default function Home() {
         >
           Check Your Health
         </Button>
-        <Button outlined onPress={() => {}} containerStyle={styles.button}>
+        <Button
+          outlined
+          onPress={onShareButtonPress}
+          containerStyle={styles.button}
+        >
           Share the App
         </Button>
         <Button outlined onPress={() => {}} containerStyle={styles.button}>
