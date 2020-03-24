e69231abba1cd4bdc800a9cc582166b1671f81ed
listiani13 <listiliz13@gmail.com>
2020-03-24 11:35:37 +0700

    fix: set has finished onboarding after user finished onboarding

--- src/helpers/useGettingStarted.ts
+++ src/helpers/useGettingStarted.ts
@@ -25,6 +25,7 @@ export default function useGettingStarted(): boolean | null {
       }
 
       setIsReturningUser(false);
+      setHasFinishedOnboarding();
     });
   }, []);
   return isReturningUser;
