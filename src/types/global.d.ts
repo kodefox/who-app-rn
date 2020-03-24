94031fa8e3c7da6289bee72df43728cc7e4a1b4b
Daniel Sukmana <daniel.sukmana@kodefox.com>
2020-03-24 14:07:12 +0700

    chore: global t function type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,25 +1,9 @@
+import { t as translate } from '../helpers/translate';
+
 declare global {
   export type ObjectOf<T> = { [key: string]: T };
 
-  // TODO: We should be able to infer this type by importing the
-  // real implementation, but I can't get that to work.
-  export type TranslatorFunction = {
-    (input: string, params?: { [key: string]: unknown }): string;
-    frag: (
-      input: string,
-      params: {
-        [key: string]:
-          | null
-          | undefined
-          | number
-          | string
-          | boolean
-          | ((s: string) => unknown);
-      },
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ) => any;
-  };
-  export const t: TranslatorFunction;
+  export const t: typeof translate;
 
   export const global: typeof globalThis;
 }
